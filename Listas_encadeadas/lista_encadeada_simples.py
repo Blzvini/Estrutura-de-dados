a72e8b616732b7ec20e72518df66dc0693357ec0
blzvini <lopes.viniciusm99@gmail.com>
2023-09-15 13:42:42 -0300

    Continuação da implementação de listas encadeadas simples

--- Listas_encadeadas/lista_encadeada_simples.py
+++ Listas_encadeadas/lista_encadeada_simples.py
@@ -1,3 +1,29 @@
 #%%
 import numpy as np
 # %%
+class No:
+    
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+        
+    def mostrar_no(self):
+        print(self.valor)
+# %%
+
+class ListaEncadeada:
+    
+    def __init__(self):
+        self.primeiro = None
+        
+    def insere_inicio(self, valor):
+        novo = No(valor)
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+    
+    def mostrar(self):
+        atual = self.primeiro
+        while atual != None:
+            atual.mostrar_no()
+            atual = atual.proximo
+        
