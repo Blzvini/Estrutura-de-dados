a5c8b750f6238a884b7da1bf72fe3662c2a0d144
blzvini <lopes.viniciusm99@gmail.com>
2023-09-25 09:18:56 -0300

    Implementação da função de exclusão

--- Listas_encadeadas/lista_encadeada_simples.py
+++ Listas_encadeadas/lista_encadeada_simples.py
@@ -22,8 +22,45 @@ class ListaEncadeada:
         self.primeiro = novo
     
     def mostrar(self):
+        if self.primeiro.proximo == None:
+            print('A lista está vazia')
+            return None
+        
         atual = self.primeiro
         while atual != None:
             atual.mostrar_no()
             atual = atual.proximo
+            
+    def excluir_inicio(self):
+        if self.primeiro.proximo == None:
+            print('A lista está vazia')
+            return None
         
+        temp = self.primeiro
+        self.primeiro = self.primeiro.proximo
+        return temp
+        
+# Insere no início
+
+#%%
+lista = ListaEncadeada()
+lista.insere_inicio(1)
+
+#%%
+lista.mostrar()
+
+#%%
+
+# Excluir do início
+
+lista = ListaEncadeada()
+lista.insere_inicio(1)
+lista.insere_inicio(2)
+lista.insere_inicio(3)
+lista.insere_inicio(4)
+lista.insere_inicio(5)
+lista.mostrar()
+
+#%%
+lista.excluir_inicio()
+lista.mostrar()
