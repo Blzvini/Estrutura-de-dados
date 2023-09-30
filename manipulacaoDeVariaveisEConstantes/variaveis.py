9b4f16b11e8e6b2980a673733b9a3a8326e6c51a
blzvini <lopes.viniciusm99@gmail.com>
2023-09-30 19:46:12 -0300

    Conteúdos de Python do curso

---
+++ manipulacaoDeVariaveisEConstantes/variaveis.py
@@ -0,0 +1,46 @@
+#%%
+
+# Variáveis inteiras
+
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+
+
+# %%
+numero
+# %%
+print(numero)
+# %%
+print(numero, numero_convidados)
+# %%
+#Variáveis float (Ponto flutuante)
+
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+# %%
+print(pi)
+print(numero_euler)
+print(escala_terremoto)
+# %%
+
+# Strings e chars
+
+letra = 'a'
+frase = 'linguagem de programação'
+frase2 = 'Python'
+# %%
+print(letra, frase)
+# %%
+print(f'Estou aprendendo {frase2}')
+# %%
+idade = int(input('Digite sua idade: '))
+print(f'sua idade é {idade}')
+# %%
+pH = float(input('Qual o pH do solo durante a última medição? '))
+print(f'o pH medido foi {pH}')
+# %%
+nome = input('Digite seu nome: ')
+print(f'Seu nome é {nome}')
+# %%
