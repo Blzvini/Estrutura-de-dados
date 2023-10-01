5cb5495365e56d0fe3502b7daa0c6d112110021e
blzvini <lopes.viniciusm99@gmail.com>
2023-10-01 17:10:58 -0300

    Resolução dos exercícios com try/execpt

--- conteudo_Python/ex02.py
+++ conteudo_Python/ex02.py
@@ -10,6 +10,23 @@ Leia a idade do usuário e classifique-o em:
 
 # Resolução ex 1
 
+def verificar_idade():
+    try:
+        idade = int(input('Informe a sua idade: '))
+        
+        if idade < 0:
+            print('Informe uma idade válida, por favor.')
+        elif(idade <= 12):
+            print('Você é uma criança ainda!')
+        elif(idade <= 17):
+            print('Você é um Adolescente')
+        elif(idade >= 18):
+            print('Você é um adulto')
+    except ValueError as e:
+        print(f"Erro: {e}")
+
+
+verificar_idade()
 #%%
 '''
 
@@ -20,5 +37,24 @@ Calcular a média de um aluno que cursou a disciplina de Programação I, a part
 - Se o aluno pegou exame, deve ser lida a nota do exame. Se a nota do exame for maior do que 6.0, está aprovado, senão; está reprovado
 
 '''
-
 # Resolução ex 2
+
+def calcular_media():
+    try:
+        n1 = float(input("Digite a nota da M1: "))
+        n2 = float(input("Digite a nota da M2: "))
+        n3 = float(input("Digite a nota da M3: "))
+        
+        media = (n1 + n2 + n3) / 3
+        
+        if media < 4:
+            print('Você está reprovado...')
+        elif media > 4.1 and media < 6:
+            print('Você está de recuperação..')
+        else:
+            print('Você está aprovado')
+    except ValueError as e:
+        print(f'Erro: {e}')
+        
+calcular_media()
+
